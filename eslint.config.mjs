32a7a26f1f9bc5a24f323382fe24960dfca8bb22
Jkoroleva <ykoroleva@t-argos.ru>
2024-09-25 08:20:38 +0300

    sprint_2: Add configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,18 @@ export default {
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "prettier", "jsx-a11y", "import"],
   env: {
     browser: true,
     es2020: true,
+    node: true,
   },
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
     "@typescript-eslint/no-unused-vars": "warn",
+    "no-unused-vars": 2,
+    "max-len": [1, 100],
+    "max-params": [2, 3],
+    'no-console': 'warn',
   },
 };
