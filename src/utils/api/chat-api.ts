9d835676f7b3c09876b7ca1cb6af135496e0ea59
Jkoroleva <ykoroleva@t-argos.ru>
2024-11-02 14:31:20 +0300

    sprint_4: Add change chat avatar

--- src/utils/api/chat-api.ts
+++ src/utils/api/chat-api.ts
@@ -33,7 +33,7 @@ export class ChatsAPI extends BaseApi {
 
   addChatAvatar(formData: FormData): Promise<unknown> {
     return this.http.put("/avatar", formData);
-}
+  }
 
   async getToken(id: number): Promise<string> {
     const response = await this.http.post<{ token: string }>(
